053e011f88f8b05f6389aa928a352f916ec4d8b5
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-05-16 21:14:42 +0100

    test shouldMapStudentToDto

--- src/test/java/com/unittesting/student/StudentMapperTest.java
+++ src/test/java/com/unittesting/student/StudentMapperTest.java
@@ -1,5 +1,6 @@
 package com.unittesting.student;
 
+import com.unittesting.school.School;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,23 @@ class StudentMapperTest {
         assertEquals(dto.age(), student.getAge());
         assertNotNull(student.getSchool());
         assertEquals(dto.SchoolId(), student.getSchool().getId());
+    }
+
+    @Test
+    public void shouldMapStudentToDto() {
+        Student student = new Student();
+        student.setFirstName("Ahmed");
+        student.setLastName("Tibe");
+        student.setEmail("ahmed.tiba.1993@gmail.com");
+        student.setAge(30);
+
+        StudentResponse studentResponse = StudentMapper.toStudentResponse(student);
+
+        assertNotNull(studentResponse);
+        assertEquals(student.getFirstName(), studentResponse.getFirstName());
+        assertEquals(student.getLastName(), studentResponse.getLastName());
+        assertEquals(student.getEmail(), studentResponse.getEmail());
+        assertEquals(student.getAge(), studentResponse.getAge());
+
     }
 }
